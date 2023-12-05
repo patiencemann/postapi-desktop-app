604f37c0373aa306a1704e80d9ef3326d9707172
Manirabona Patience <hseal419@gmail.com>
2023-12-05 17:17:22 +0200

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'com.patienceman.postapi',
+        productName: 'Postapi',
+        win: {
+          icon: 'build/icons/icon.ico'
+        },
+        mac: {
+          icon: 'build/icons/icon.icns'
+        },
+        linux: {
+          icon: 'build/icons/'
+        }
+      }
+    }
+  }
 })
